3c8af6362d06fe05db5b07f3e7e9fdd18ecc8bb8
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-03-24 23:02:34 -0500

    Started Development

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { Center } from '@mantine/core'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  AppShell,
+  Button,
+  Center,
+  Header,
+  UnstyledButton,
+} from "@mantine/core";
+import { Link, Route, Router, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <Center>
-        Pagina En Construccion
-      </Center>
+      <Header height={60}>
+        <Center>
+          <Link to={"/"}>
+            <UnstyledButton p={10}>Inicio</UnstyledButton>
+          </Link>
+          <Link to={"/proyectos"}>
+            <UnstyledButton p={10}>Proyectos</UnstyledButton>
+          </Link>
+          <Link to={"/productos"}>
+            <UnstyledButton p={10}>Productos</UnstyledButton>
+          </Link>
+        </Center>
+        <Routes>
+          <Route path="/" element={<h1>Inicio</h1>} />
+          <Route path="/proyectos" element={<h1>Proyectos</h1>} />
+          <Route path="/productos" element={<h1>Productos</h1>} />
+        </Routes>
+      </Header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
