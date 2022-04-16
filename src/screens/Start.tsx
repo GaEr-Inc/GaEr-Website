63d5c5b72adc84522522aa2f4eb03e4eb510629b
Jaoc <javier.ossa@correounivalle.edu.co>
2022-04-16 03:14:36 -0500

    Start Page Updated

--- src/screens/Start.tsx
+++ src/screens/Start.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
 
 function Start() {
   return (
-    <div className="ml-20">Smthing to see</div>
-  )
+    <div className="ml-20 mr-20 pt-6">
+      <div className="bg-purple-800 w-full h-72 rounded-lg">
+        <div className="pt-10 pl-10">
+          <span className="text-white text-6xl mt-60">Tú lo propones</span>
+        </div>
+        <div className="pt-2">
+          <span className=" pt-10 pl-10 text-white text-2xl">
+            Nosotros lo hacemos realidad
+          </span>
+        </div>
+      </div>
+      <div className="pt-2">
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id
+          blandit urna. Pellentesque id egestas magna. Integer ornare porta
+          ante, a congue metus varius a. Morbi arcu justo, suscipit nec vehicula
+          sit amet, feugiat sed dui. Sed at nisl non risus venenatis congue sit
+          amet ut libero. Nam sodales, elit nec molestie eleifend, felis est
+          consectetur tortor, eu fringilla augue lorem sit amet neque. Proin ut
+          accumsan enim. Vestibulum non enim eget diam molestie dapibus. Nullam
+          eros arcu, commodo in tempor id, tincidunt consequat est. Sed
+          pellentesque nunc sit amet gravida eleifend. Proin vel mauris sit amet
+          lacus dignissim tincidunt sit amet ut massa. Maecenas pharetra tempus
+          mattis. Quisque ante ligula, blandit ac vulputate a, tempus et erat.
+          Curabitur vitae tellus velit. Praesent aliquet pellentesque convallis.
+          Pellentesque gravida, mi feugiat fermentum viverra, ante ipsum
+          dignissim ligula, volutpat condimentum diam lacus et massa. Fusce
+          semper ligula vitae eros dictum, suscipit fringilla nunc pellentesque.
+          Nullam fermentum feugiat vulputate. Nunc hendrerit porttitor
+          efficitur. Nulla eget dui aliquam, tempor libero quis, tempus diam.
+          Integer semper sem odio, eget consectetur enim mattis sollicitudin.
+          Praesent varius, velit id interdum aliquam, odio felis vehicula
+          turpis, lobortis malesuada sem dui ac tortor. Quisque urna nulla,
+          porttitor ultricies elementum eu, posuere at neque. Suspendisse eget
+          purus turpis. Suspendisse mi sapien, molestie ut ligula at, sagittis
+          congue enim. Sed tincidunt ante purus, eu ultrices velit scelerisque
+          id. Nullam aliquam efficitur arcu in feugiat. Pellentesque euismod
+          quam eget nisl lacinia dictum. Praesent pretium tristique vestibulum.
+          Curabitur aliquam fringilla nisl eget ornare. Vestibulum aliquet ex id
+          arcu convallis rutrum. Pellentesque laoreet efficitur fringilla. Nulla
+          imperdiet commodo turpis, id finibus lacus mattis non. Morbi congue
+          velit eget urna rhoncus varius nec ut elit. Sed ac semper purus. Sed
+          facilisis velit vel accumsan pellentesque. Sed efficitur accumsan
+          elit, id consequat sem fringilla ut. Vestibulum ipsum lacus, porttitor
+          laoreet varius a, lobortis vel tellus. Nam gravida, augue suscipit
+          facilisis luctus, risus elit mollis leo, eget commodo erat felis nec
+          sem. Phasellus velit lacus, mollis nec magna id, facilisis blandit
+          felis. Nulla vel turpis luctus, tempor metus sed, ultrices justo.
+          Etiam nec orci nec tortor bibendum pharetra. Donec a quam ut velit
+          sagittis ultricies vel sed neque. Phasellus ultrices congue libero, in
+          tincidunt magna vehicula eget. Ut blandit quam leo, sit amet interdum
+          nunc gravida in. Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Quisque vitae justo eleifend, blandit enim vel, ullamcorper
+          magna. Cras nisi est, vulputate sed porta sit amet, dignissim ut
+          lacus. Vivamus tincidunt magna tortor, et malesuada lacus tincidunt
+          ut. Maecenas volutpat convallis ex vitae sodales.
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default Start
+export default Start;
